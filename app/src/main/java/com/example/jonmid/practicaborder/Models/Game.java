7b1c6097f510fa703e466aaf48936844b955bf08
pc714 <pc714@iucesmag.edu.co>
2018-04-26 17:26:09 -0500

    2

--- app/src/main/java/com/example/jonmid/practicaborder/Models/Game.java
+++ app/src/main/java/com/example/jonmid/practicaborder/Models/Game.java
@@ -5,27 +5,3 @@ public class Game {
     private  String character;
     private  String gameSeries;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCharacter() {
-        return character;
-    }
-
-    public void setCharacter(String character) {
-        this.character = character;
-    }
-
-    public String getGameSeries() {
-        return gameSeries;
-    }
-
-    public void setGameSeries(String gameSeries) {
-        this.gameSeries = gameSeries;
-    }
-}

9ebc01ee16175279ac36232a797ad29c88235f2b
pc714 <pc714@iucesmag.edu.co>
2018-04-26 17:24:28 -0500

    2

--- app/src/main/java/com/example/jonmid/practicaborder/Models/Food.java
+++ app/src/main/java/com/example/jonmid/practicaborder/Models/Food.java
@@ -1,4 +1,13 @@
 package com.example.jonmid.practicaborder.Models;
 
 public class Food {
+    private  String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
--- app/src/main/java/com/example/jonmid/practicaborder/Models/Game.java
+++ app/src/main/java/com/example/jonmid/practicaborder/Models/Game.java
@@ -1,4 +1,31 @@
 package com.example.jonmid.practicaborder.Models;
 
 public class Game {
+    private  String name;
+    private  String character;
+    private  String gameSeries;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCharacter() {
+        return character;
+    }
+
+    public void setCharacter(String character) {
+        this.character = character;
+    }
+
+    public String getGameSeries() {
+        return gameSeries;
+    }
+
+    public void setGameSeries(String gameSeries) {
+        this.gameSeries = gameSeries;
+    }
 }
